35b35de8dee717c5dc78846a333dc5a3622f349b
Thomas Bereczky <thomasbereczky@sysopsnet.com>
2021-02-26 18:44:53 +0100

    Small changes

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,22 +8,6 @@ class Footer extends React.PureComponent {
 	render() {
 		return (
 			<Grid id="footer">
-				<Row>
-					<Col md={3}>
-						<LumensRates />
-					</Col>
-					<Col mdOffset={7} md={2}>
-						<a href="https://kinecosystem.org/">
-							<img
-								src={`${process.env.PUBLIC_URL}/kin.ico`}
-								alt="kin"
-								height={20}
-								width={20}
-							/>
-							kinecosystem.org
-						</a>
-					</Col>
-				</Row>
 			</Grid>
 		)
 	}
